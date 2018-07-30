16e118b19986ccd29139c4d091415e59d356dbf4
Charles Fries <charliefries@icloud.com>
2018-07-29 22:01:36 -0700

    Added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    var target = this.hash, $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+});

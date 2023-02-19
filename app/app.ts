b56b157a169a1630630bc89d7d234f906670ad9d
Charles Fries <charliefries@icloud.com>
2023-02-19 14:19:13 -0800

    Install mixpanel

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'charlesfries/config/environment';
+import mixpanel from 'mixpanel-browser';
 import 'bootstrap';
 
 export default class App extends Application {
@@ -11,3 +12,7 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+
+mixpanel.init('f537ea51ddb0c79c7e5f339f832e5267', {
+  debug: 'development' === config.environment,
+});
--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,14 +1,23 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import mixpanel from 'mixpanel-browser';
 
 import type Intl from 'ember-intl/services/intl';
+import type RouterService from '@ember/routing/router-service';
 
 export default class ApplicationRoute extends Route {
   @service declare intl: Intl;
+  @service declare router: RouterService;
 
   constructor(...args: any[]) {
     super(...args);
 
     this.intl.setLocale('en-us');
+
+    this.router.on('routeDidChange', () => {
+      const page = this.router.currentURL;
+      const title = this.router.currentRouteName || 'unknown';
+      mixpanel.track('Page view', { page, title });
+    });
   }
 }

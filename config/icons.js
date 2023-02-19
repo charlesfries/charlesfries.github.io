36158effdfa88aee597fd4e7bc455dd76edbcd13
Charles Fries <charliefries@icloud.com>
2023-02-19 15:29:46 -0800

    Add config/icons.js

---
+++ config/icons.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function () {
+  // https://github.com/FortAwesome/ember-fontawesome#subsetting-icons
+
+  return {
+    'free-brands-svg-icons': ['github', 'instagram', 'linkedin', 'twitter'],
+    'free-solid-svg-icons': [
+      'code-fork',
+      'envelope',
+      'redo',
+      'sort-amount-down',
+      'sort-amount-up',
+      'star',
+      'triangle-exclamation',
+    ],
+  };
+};

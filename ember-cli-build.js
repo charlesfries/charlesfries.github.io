66f8abb4a9d20c8887d370b2112b11f347a374b4
Charles Fries <charliefries@icloud.com>
2026-04-12 16:46:31 -0700

    chore: uninstall sass (#1)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
-    sassOptions: {
-      includePaths: ['node_modules/bootstrap/scss'],
-      onlyIncluded: true,
-    },
   });
+
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };

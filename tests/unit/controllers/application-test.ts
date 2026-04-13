4183dad01b9479a4f3fbef67e4e0cc60e543f819
Charles Fries <charliefries@icloud.com>
2026-04-13 03:27:18 -0700

    Ch

--- tests/unit/controllers/application-test.ts
+++ tests/unit/controllers/application-test.ts
@@ -4,7 +4,6 @@ import { setupTest } from 'charlesfries/tests/helpers';
 module('Unit | Controller | application', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
     const controller = this.owner.lookup('controller:application');
     assert.ok(controller);
--- tests/unit/controllers/index-test.ts
+++ tests/unit/controllers/index-test.ts
@@ -1,12 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'charlesfries/tests/helpers';
-import type IndexController from 'charlesfries/controllers';
 
 module('Unit | Controller | index', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    const controller = this.owner.lookup('controller:index') as IndexController;
+    const controller = this.owner.lookup('controller:index');
     assert.ok(controller);
   });
 });
